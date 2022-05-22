4a72786628685d217948ba4905d25e66b5c9bc9e
José Manuel Hernández Mayorga <manuelmayorga99@gmail.com>
2022-05-22 12:10:47 -0500

    Request API done, and working awesome from Postman

--- app.js
+++ app.js
@@ -6,7 +6,46 @@ app.use(express.json()); //Indicamos que usaremos JSON
 //Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000;
 
+// HTTP METHODS
+//Get all explorers
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL REQUEST ${new Date()}`);
+    const explorer1 = {id: 1, name: "Manuel1"};
+    const explorer2 = {id: 2, name: "Manuel2"};
+    const explorer3 = {id: 3, name: "Manuel3"};
+    const explorer4 = {id: 4, name: "Manuel4"};
+    const explorers = [explorer1, explorer2, explorer3, explorer4];
+    res.status(200).json(explorers);
+})
+//Get single explorer with ID
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`);
+    console.log(`Getting explorer with id ${new Date()}`);
+    const explorer = {id: 1, name: "Manuel"};
+    res.status(200).json(explorer);
+})
+//Post, create explorer
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`);
+    const requestBody = req.body //Parametros de un cliente
+    res.status(201).json({message: "Created"});
+})
+//Put, update explorer
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`);
+    console.log(`Update explorer with id ${req.params.id}`);
+    const requestBody = req.body //Parametros del cliente
+    res.status(200).json({message: "Updated!"});
+})
+//Delete, delete a explorer
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`);
+    console.log(`Delete explorer with id ${req.params.id}`);
+    const requestBody = req.body //Parametros de un cliente
+    res.status(200).json({message: "Deleted"});
+})
+
 //Con esto inicializamos esta app
-app.listen(post, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
